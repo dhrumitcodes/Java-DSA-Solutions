7a90e3a9ac522d73c73893496d1acb64722d5443
Dhrumit Choudhary <dhrumitchoudhary123@gmail.com>
2026-07-29 20:47:03 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 700889-reverse-an-array-GFG/700889-reverse-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            
+            i++;
+            j--;
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
