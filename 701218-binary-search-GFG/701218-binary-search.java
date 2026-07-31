d6ea93ae0b262429038eb2234b3aa058f1538c45
Dhrumit Choudhary <dhrumitchoudhary123@gmail.com>
2026-07-31 16:36:12 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701218-binary-search-GFG/701218-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean binarySearch(int[] arr, int k) {
+      
+      
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == k) {
+                return true; 
+            }
+        }
+        
+        return false; 
+    }
+}
+        
+ 
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
