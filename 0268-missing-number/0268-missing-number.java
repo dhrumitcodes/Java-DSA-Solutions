9b4a6b6b45f86a6db281d6a5ec811f34aff1207c
Dhrumit Choudhary <dhrumitchoudhary123@gmail.com>
2026-08-01 14:59:04 +0530

    Time: 0 ms (100%), Space: 47.5 MB (32.95%) - LeetHub - Time: O(n), Space: O(1)

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
+        int xor = nums.length;
         
-
-        int expectedSum = n * (n + 1) / 2;
-    
-        int actualSum = 0;
-        for (int num : nums) {
-            actualSum += num;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i]; 
         }
         
-        return expectedSum - actualSum;
+        return xor;
     }
 }
 
