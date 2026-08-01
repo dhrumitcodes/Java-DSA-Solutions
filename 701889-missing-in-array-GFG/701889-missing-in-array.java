49cced4d6bc1f6be28c3daa8a57424014653b587
Dhrumit Choudhary <dhrumitchoudhary123@gmail.com>
2026-08-01 15:00:19 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701889-missing-in-array-GFG/701889-missing-in-array.java
@@ -0,0 +1,18 @@
+class Solution {
+int missingNum(int arr[]) {
+        long n = arr.length + 1;
+        
+        long expectedSum = n * (n + 1) / 2;
+        
+        long actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
+        }
+        
+        return (int) (expectedSum - actualSum);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
