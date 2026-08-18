5bc65d04c8e0882532363199e103ff5c33c0da66
Dhrumit Choudhary <dhrumitchoudhary123@gmail.com>
2026-08-18 15:31:13 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 712537-pass-by-reference-and-value-GFG/712537-pass-by-reference-and-value.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        int newA=a+1;
+        int newB=b+2;
+        
+        return new int[]{newA,newB};
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
