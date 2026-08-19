8f51b697f2423bc0d0f19202b05ff7721f1a3529
Dhrumit Choudhary <dhrumitchoudhary123@gmail.com>
2026-08-19 16:16:31 +0530

    Added solution

---
+++ 706210-reverse-bits-GFG/706210-reverse-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        while (n > 0) {
+            int lastBit = n & 1;       
+            result = (result << 1) | lastBit; 
+            n = n >> 1;                
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
