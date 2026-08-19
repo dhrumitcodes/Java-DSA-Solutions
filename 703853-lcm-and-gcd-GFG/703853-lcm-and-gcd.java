fe0c660d61e7032c64373b45960bd7b38273350e
Dhrumit Choudhary <dhrumitchoudhary123@gmail.com>
2026-08-19 16:39:10 +0530

    Added solution

---
+++ 703853-lcm-and-gcd-GFG/703853-lcm-and-gcd.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        int gcdVal = gcd(a, b);
+        return (a * b) / gcdVal;
+    }
+
+    static int[] lcmAndGcd(int a, int b) {
+        int lcmVal = lcm(a, b);
+        int gcdVal = gcd(a, b);
+        int[] result = {lcmVal, gcdVal};
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
